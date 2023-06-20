c9c2a03dfa637a75a020714d1fcb3f3a6e6aee39
LBQTrung <lebaquoctrung@gmail.com>
2023-06-20 21:10:16 +0700

    7. Finish basic interface

--- js/script.js
+++ js/script.js
@@ -34,3 +34,36 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 }
+
+// Scroll reveal
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', {
+    origin: 'top'
+})
+
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', {
+    origin: 'bottom'
+})
+
+ScrollReveal().reveal('.home-content h1, .about-img', {
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.home-content p, .about-content', {
+    origin: 'right'
+})
+
+// Typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Data Engineer', 'AI Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
